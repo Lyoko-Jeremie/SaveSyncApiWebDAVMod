5a775cae3e36c46fd9f84ccc01fe44e516d97ac2
LyokoJeremie <lucheng989898@gmail.com>
2023-12-08 18:58:36 +0800

    init

---
+++ src/init.ts
@@ -0,0 +1,4 @@
+import {SaveSyncApiWebDAV} from "./SaveSyncApiWebDAV";
+
+window.modSaveSyncApiWebDAV = new SaveSyncApiWebDAV(window.modSC2DataManager, window.modUtils);
+window.modSaveSyncApiWebDAV.init();
